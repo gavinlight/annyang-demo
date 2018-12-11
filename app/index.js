f433f2e80823b52d3cf35af05ce79252e0953cb0
Gavin Ligthart <gavin@Gavins-Macbook.local>
2018-12-10 19:54:02 -0800

    Add chaining conversations

--- app/index.js
+++ app/index.js
@@ -1,36 +1,36 @@
 window.speechSynthesis.onvoiceschanged = function() {
   annyang.start();
-  addConvo(commands);
+
+  addConvo('Hi :first', (first) => {
+    say(`Goodmorning ${first}. Welcome in your Greenwheels for today. How is you car looking?`, () => {
+      addConvo(':response', (response) => {
+        say(`${response}? That's not good. We will contact you ASAP.`)
+      });
+    });
+  });
 };
 
-const commands = {
-  ':first': (first) => {
-    say(`Hello ${first}. Hoe gaat het?`);
-  },
-};
-
-const addConvo = (commands) => {
-  annyang.addCommands(mapCallbacks(commands));
+const addConvo = (trigger, callback) => {
+  annyang.addCommands({
+    [trigger]: (...params) => {
+      callback(...params);
+      annyang.removeCommands(trigger);
+    }
+  });
 }
 
-const mapCallbacks = (commands) => {
-  return Object.assign(
-    {},
-    ...Object.keys(commands).map(trigger => ({
-      [trigger]: (...params) => {
-        commands[trigger](...params);
-        annyang.removeCommands(trigger);
-      }
-    }))
-  );
-};
 
-const say = (msg) => {
+
+const say = (msg, callback) => {
   const message = new SpeechSynthesisUtterance();
-  message.voice = speechSynthesis.getVoices().find(voice => voice.name === 'Ting-Ting');
+  message.voice = speechSynthesis.getVoices().find(voice => voice.name === 'Daniel');
   message.text = msg;
   message.rate = 10;
   message.pitch = 2;
 
+  window.messages = [];
+  messages.push(message);
+
+  message.addEventListener('end', () => { if(callback) callback(); });
   window.speechSynthesis.speak(message);
 }
