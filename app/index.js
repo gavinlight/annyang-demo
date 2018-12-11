5fc2731e3e9d1f01528336e84389f1f6d43f05b5
Gavin Ligthart <gavin@Gavins-Macbook.local>
2018-12-10 20:24:11 -0800

    Cancel speach before new speach, so it doesn't overlap

--- app/index.js
+++ app/index.js
@@ -20,8 +20,10 @@ const addConvo = (trigger, callback) => {
 }
 
 const say = (msg, callback) => {
+  window.speechSynthesis.cancel();
+
   const message = new SpeechSynthesisUtterance();
-  message.voice = speechSynthesis.getVoices().find(voice => voice.name === 'Daniel');
+  message.voice = window.speechSynthesis.getVoices().find(voice => voice.name === 'Daniel');
   message.text = msg;
   message.rate = 10;
   message.pitch = 2;
